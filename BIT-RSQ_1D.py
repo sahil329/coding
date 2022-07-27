7113aa8a5ca4daa1514a026a322eb6f2d6358978
Sahil Gevariya <64141966+sahil329@users.noreply.github.com>
2022-07-27 19:37:02 +0530

    Fenwick Tree/Binary Search Tree
    Better data structure for range some query compar to Segment Tree.

---
+++ BIT-RSQ_1D.py
@@ -0,0 +1,25 @@
+class BIT:
+    def __init__(self,nums):
+        self.n = len(nums)
+        self.tree = ["*"]+copy.deepcopy(nums)
+        self.construct()
+        
+    def construct(self):
+        for i in range(1,self.n+1):
+            p = i + (i & (-i))
+            if p<=self.n:
+                self.tree[p] += self.tree[i]
+        # print(self.tree)
+    
+    def update(self,i,delta):
+        while i<=self.n:
+            self.tree[i] += delta
+            i += (i & (-i))
+        # print(self.tree,delta)
+        
+    def getSum(self,i):
+        sum = 0
+        while i>0:
+            sum += self.tree[i]
+            i -= (i & (-i))   
+        return sum
